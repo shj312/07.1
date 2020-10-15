7359c442914ae2bc737466710c9b2f1769558e80
shj312 <sshj312@naver.com>
2020-10-15 16:02:29 +0900

    07.4

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void f(void);
 
-int main(int argc, char *argv[]) 
+int i;
+int main(void)
 {
- int i;
- 
- for(i=0;i<5;i++)
- {
- 	int temp = 1;
- 	printf("temp = %d\n", temp);
- 	temp++;
- }
- return 0;
+	for(i=0;i<5;i++)
+	{
+		f();
+	}
+	return 0;
+}
+void f(void)
+{
+	for(i=0;i<10;i++)
+	 printf("#");
 }
